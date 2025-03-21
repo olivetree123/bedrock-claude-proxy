6b520ce8ab7bbf8cdc42ed9acfb34689f3161a6f
gaojian <A24619@autelsz.com>
2025-03-21 10:44:40 +0800

    fix bug

--- pkg/http.go
+++ pkg/http.go
@@ -273,7 +273,7 @@ func (this *HTTPService) HandleMessageComplete(writer http.ResponseWriter, reque
 						}
 
 						// 记录使用情况
-						quota := int((float64(inputTokens) * ModelMetaMap[req.Model].ModelRatio + float64(outputTokens)*ModelMetaMap[req.Model].CompletionRatio))
+						quota := int(float64(inputTokens)*ModelMetaMap[req.Model].ModelRatio + float64(outputTokens)*ModelMetaMap[req.Model].CompletionRatio)
 						if err := models.CreateUsage(this.db, apiKeyName, apiKeyValue, req.Model,
 							inputTokens, outputTokens, quota); err != nil {
 							log.Logger.Errorf("Failed to log API usage: %v", err)
@@ -308,7 +308,7 @@ func (this *HTTPService) HandleMessageComplete(writer http.ResponseWriter, reque
 		}
 
 		// 记录使用情况
-		quota := int((float64(resp.Usage.InputTokens) * ModelMetaMap[resp.Model].ModelRatio + float64(resp.Usage.OutputTokens)*ModelMetaMap[resp.Model].CompletionRatio))
+		quota := int(float64(resp.Usage.InputTokens)*ModelMetaMap[resp.Model].ModelRatio + float64(resp.Usage.OutputTokens)*ModelMetaMap[resp.Model].CompletionRatio)
 		if err := models.CreateUsage(this.db, apiKeyName, apiKeyValue, resp.Model,
 			resp.Usage.InputTokens, resp.Usage.OutputTokens, quota); err != nil {
 			log.Logger.Errorf("Failed to log API usage: %v", err)
